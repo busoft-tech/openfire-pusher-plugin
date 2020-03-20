150082760a2a97e80199196a4c0a5aa4811fa67f
Ahmet Enes Bayraktar <bayraktarahmetenes@gmail.com>
2020-03-20 20:44:20 +0300

    db changes

--- src/main/database/pusher_mysql.sql
+++ src/main/database/pusher_mysql.sql
@@ -2,7 +2,7 @@ CREATE TABLE ofPusher (
   username              VARCHAR(64)     NOT NULL,
   resource              VARCHAR(64)     NOT NULL,
   token                 VARCHAR(1024)   NOT NULL
+  PRIMARY KEY (username, resource)
 );
-CREATE INDEX ofPusher_idx ON ofPusher (username);
 
 INSERT INTO ofVersion (name, version) VALUES ('pusher', 1);
